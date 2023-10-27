b908ae6f277b6bae4a76e3c8e20e323d05d873a5
gabriel <mmmgabrieldeandrade@gmail.com>
2023-10-27 11:16:40 -0300

    Resolvido o Extremamente Básico

---
+++ src/main/java/extremamentebasico/Main.java
@@ -0,0 +1,14 @@
+package extremamentebasico;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+        int a = tec.nextInt();
+        int b = tec.nextInt();
+
+        System.out.println("X = " + (a + b));
+    }
+}
