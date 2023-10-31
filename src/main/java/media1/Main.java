7ed675ca8edddc068901bc7fadbb53a7d6af90b2
gabriel <mmmgabrieldeandrade@gmail.com>
2023-10-31 09:02:34 -0300

    Resolvido a Diferença

---
+++ src/main/java/media1/Main.java
@@ -0,0 +1,16 @@
+package media1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner t = new Scanner(System.in);
+        int a = t.nextInt();
+        int b = t.nextByte();
+        int c = t.nextInt();
+        int d = t.nextInt();
+        System.out.println("DIFERENCA = " + (a*b - c*d));
+    }
+
+}
