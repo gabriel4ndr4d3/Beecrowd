333fc317aa32a15eb419dca32fe4d59b02a59065
gabriel <mmmgabrieldeandrade@gmail.com>
2023-11-01 09:52:05 -0300

    Resolvido a esfera

---
+++ src/main/java/esfera/Main.java
@@ -0,0 +1,16 @@
+package esfera;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner t = new Scanner(System.in);
+
+        double raio = t.nextDouble();
+        double pi = 3.14159;
+
+        System.out.printf("VOLUME = %.3f\n", Math.pow(raio, 3) * pi * (4.0 / 3));
+
+    }
+}
