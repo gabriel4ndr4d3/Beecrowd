38fdce34e6509f18b232cb571b02c8d156421942
gabriel <mmmgabrieldeandrade@gmail.com>
2023-11-03 09:31:00 -0300

    Resolvido a Distância

---
+++ src/main/java/distancia/Main.java
@@ -0,0 +1,15 @@
+package distancia;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner t = new Scanner(System.in);
+
+        int x = t.nextInt();
+
+        System.out.println(x * 2 + " minutos");
+
+    }
+}
