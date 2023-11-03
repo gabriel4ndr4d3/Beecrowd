6bc04511b1fa7ca2809f3e9a4220e46d23ee0948
gabriel <mmmgabrieldeandrade@gmail.com>
2023-11-03 11:08:35 -0300

    Melhorado o Maior

--- src/main/java/omaior/Main.java
+++ src/main/java/omaior/Main.java
@@ -1,7 +1,7 @@
 package omaior;
 
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,13 +11,12 @@ public class Main {
         int b = t.nextInt();
         int c = t.nextInt();
 
-        int first = (a + b + Math.abs(a - b)) / 2;
+        List<Integer> values = Arrays.asList(a, b, c);
 
-        if (first > c) {
-            System.out.println(first + " eh o maior");
+        values.sort(Integer::compare);
 
-        } else {
-            System.out.println(c + " eh o maior");
-        }
+        int better = values.get(values.size() - 1);
+
+        System.out.println(better + " eh o maior");
     }
 }
