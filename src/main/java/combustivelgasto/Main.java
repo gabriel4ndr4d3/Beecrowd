7f02dac188563bc9570f4b5aadbc7f65ee927697
gabriel <mmmgabrieldeandrade@gmail.com>
2023-11-03 10:04:42 -0300

    Resolvido o gasto de combustivel

---
+++ src/main/java/combustivelgasto/Main.java
@@ -0,0 +1,16 @@
+package combustivelgasto;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner t = new Scanner(System.in);
+
+        double tempo = t.nextDouble();
+        double velocidade = t.nextDouble();
+
+        System.out.printf("%.3f\n", tempo * velocidade / 12);
+
+    }
+}
