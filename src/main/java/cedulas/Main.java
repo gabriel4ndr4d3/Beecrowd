8721c058f79cfc7711d5aaa0bf934a9ec37da745
gabriel <mmmgabrieldeandrade@gmail.com>
2023-11-07 10:29:26 -0300

    Melhorando as Cédulas

--- src/main/java/cedulas/Main.java
+++ src/main/java/cedulas/Main.java
@@ -11,36 +11,15 @@ public class Main {
 
         System.out.println(value);
 
-        int cedulas = value / 100;
-        value -= cedulas * 100;
+        int[] cedulas = { 100, 50, 20, 10, 5, 2, 1};
 
-        System.out.println(cedulas + " nota(s) de R$ 100,00");
+        for (int valorCedula : cedulas) {
 
-        int cedulas2 = value / 50;
-        value -= cedulas2 * 50;
+            int numeroDeCedula = value / valorCedula;
 
-        System.out.println(cedulas2 + " nota(s) de R$ 50,00");
+            value -= numeroDeCedula * valorCedula;
 
-        int cedulas3 = value / 20;
-        value -= cedulas3 * 20;
-
-        System.out.println(cedulas3 + " nota(s) de R$ 20,00");
-
-        int cedulas4 = value / 10;
-        value -= cedulas4 * 10;
-
-        System.out.println(cedulas4 + " nota(s) de R$ 10,00");
-
-        int cedulas5 = value / 5;
-        value -= cedulas5 * 5;
-
-        System.out.println(cedulas5 + " nota(s) de R$ 5,00");
-
-        int cedulas6 = value / 2;
-        value -= cedulas6 * 2;
-
-        System.out.println(cedulas6 + " nota(s) de R$ 2,00");
-
-        System.out.println(value + " nota(s) de R$ 1,00");
+            System.out.printf("%s nota(s) de R$ %s,00\n", numeroDeCedula, valorCedula);
+        }
     }
 }
