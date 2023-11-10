824962841e0922aadca587780823dd16741f3dc5
gabriel <mmmgabrieldeandrade@gmail.com>
2023-11-10 10:08:30 -0300

    Resolvido o exercício Distância Entre Dois Pontos

---
+++ src/main/java/distanciadoispontos/Main.java
@@ -0,0 +1,20 @@
+package distanciadoispontos;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner t = new Scanner(System.in);
+
+        double x1 = Double.parseDouble(t.next());
+        double y1 = Double.parseDouble(t.next());
+
+        double x2 = Double.parseDouble(t.next());
+        double y2 = Double.parseDouble(t.next());
+
+        double result = Math.sqrt(Math.pow(x2 - x1, 2) + (Math.pow(y2 - y1, 2)));
+
+        System.out.printf("%.4f\n", result);
+    }
+}
