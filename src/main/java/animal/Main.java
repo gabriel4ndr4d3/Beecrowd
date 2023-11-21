04d28e612a9544c80f5868a99ecfbedbf66c2ef3
gabriel <mmmgabrieldeandrade@gmail.com>
2023-11-21 09:27:41 -0300

    Melhorando o exercício Animal

--- src/main/java/animal/Main.java
+++ src/main/java/animal/Main.java
@@ -14,24 +14,31 @@ public class Main {
 
         if (word1.equals("vertebrado") && word2.equals("ave") && word3.equals("carnivoro")) {
             System.out.println("aguia");
+            return;
         }
         if (word1.equals("vertebrado") && word2.equals("ave") && word3.equals("onivoro")) {
             System.out.println("pomba");
+            return;
         }
         if (word1.equals("vertebrado") && word2.equals("mamifero") && word3.equals("onivoro")) {
             System.out.println("homem");
+            return;
         }
         if (word1.equals("vertebrado") && word2.equals("mamifero") && word3.equals("herbivoro")) {
             System.out.println("vaca");
+            return;
         }
         if (word1.equals("invertebrado") && word2.equals("inseto") && word3.equals("hematofago")) {
             System.out.println("pulga");
+            return;
         }
         if (word1.equals("invertebrado") && word2.equals("inseto") && word3.equals("herbivoro")) {
             System.out.println("lagarta");
+            return;
         }
         if (word1.equals("invertebrado") && word2.equals("anelideo") && word3.equals("hematofago")) {
             System.out.println("sanguessuga");
+            return;
         }
         if (word1.equals("invertebrado") && word2.equals("anelideo") && word3.equals("onivoro")) {
             System.out.println("minhoca");
